bfeacc233171e2ef0d40e153484f17052d262b2f
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-02-14 10:31:27 +0900

    refactor: ほんのりとしたリファクタ、ほんのリファクタ

--- infer/crates/e2k-rs/tasks/plot_benchmark.py
+++ infer/crates/e2k-rs/tasks/plot_benchmark.py
@@ -21,17 +21,27 @@ rust_c2k_iters = rust_c2k_benchmark["iters"]
 rust_p2k_benchmarks = rust_p2k_benchmark["times"]
 rust_p2k_iters = rust_p2k_benchmark["iters"]
 
+# unit: ms/iter
+python_c2k_benchmarks_ms = [x * 1000 for x in python_c2k_benchmarks]
+python_p2k_benchmarks_ms = [x * 1000 for x in python_p2k_benchmarks]
+rust_c2k_benchmarks_ms = [
+    x / 1000 / 1000 / i for x, i in zip(rust_c2k_benchmarks, rust_c2k_iters)
+]
+rust_p2k_benchmarks_ms = [
+    x / 1000 / 1000 / i for x, i in zip(rust_p2k_benchmarks, rust_p2k_iters)
+]
+
 fig = plt.figure(figsize=(12, 6))
 
 c2k_hist_fig = fig.add_subplot(2, 2, 1)
 c2k_hist_fig.hist(
-    list(map(lambda x: x * 1000, python_c2k_benchmarks)),
+    python_c2k_benchmarks_ms,
     bins=10,
     alpha=0.5,
     label="Python C2K",
 )
 c2k_hist_fig.hist(
-    [x / 1000 / 1000 / i for x, i in zip(rust_c2k_benchmarks, rust_c2k_iters)],
+    rust_c2k_benchmarks_ms,
     bins=10,
     alpha=0.5,
     label="Rust C2K",
@@ -43,12 +53,12 @@ c2k_hist_fig.legend()
 c2k_fig = fig.add_subplot(2, 2, 2)
 c2k_fig.scatter(
     range(len(python_c2k_benchmarks)),
-    list(map(lambda x: x * 1000, python_c2k_benchmarks)),
+    python_c2k_benchmarks_ms,
     label="Python C2K",
 )
 c2k_fig.scatter(
     range(len(rust_c2k_benchmarks)),
-    [x / 1000 / 1000 / i for x, i in zip(rust_c2k_benchmarks, rust_c2k_iters)],
+    rust_c2k_benchmarks_ms,
     label="Rust C2K",
 )
 c2k_fig.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f"{x:.0f}ms"))
@@ -57,13 +67,13 @@ c2k_fig.legend()
 
 p2k_hist_fig = fig.add_subplot(2, 2, 3)
 p2k_hist_fig.hist(
-    list(map(lambda x: x * 1000, python_p2k_benchmarks)),
+    python_p2k_benchmarks_ms,
     bins=10,
     alpha=0.5,
     label="Python P2K",
 )
 p2k_hist_fig.hist(
-    [x / 1000 / 1000 / i for x, i in zip(rust_p2k_benchmarks, rust_p2k_iters)],
+    rust_p2k_benchmarks_ms,
     bins=10,
     alpha=0.5,
     label="Rust P2K",
@@ -75,12 +85,12 @@ p2k_hist_fig.legend()
 p2k_fig = fig.add_subplot(2, 2, 4)
 p2k_fig.scatter(
     range(len(python_p2k_benchmarks)),
-    list(map(lambda x: x * 1000, python_p2k_benchmarks)),
+    python_p2k_benchmarks_ms,
     label="Python P2K",
 )
 p2k_fig.scatter(
     range(len(rust_p2k_benchmarks)),
-    [x / 1000 / 1000 / i for x, i in zip(rust_p2k_benchmarks, rust_p2k_iters)],
+    rust_p2k_benchmarks_ms,
     label="Rust P2K",
 )
 p2k_fig.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f"{x:.0f}ms"))
