c7828edfd75266027e3b1225b6296d548676e035
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-02-14 11:35:06 +0900

    ci: CIを追加

--- infer/crates/e2k-js/js/build.ts
+++ infer/crates/e2k-js/js/build.ts
@@ -4,7 +4,7 @@ import * as tsdown from "tsdown";
 
 await fs.promises.rm("./dist", { recursive: true, force: true });
 const wasm = await fs.promises.readFile("../pkg/e2k_js_bg.wasm");
-const compressed = gzipSync(wasm, {});
+const compressed = gzipSync(wasm, { level: 9 });
 const base64 = Buffer.from(compressed).toString("base64");
 await fs.promises.writeFile(
   "./src/e2k_js_bg.wasm.js",
@@ -12,14 +12,14 @@ await fs.promises.writeFile(
 );
 await fs.promises.writeFile(
   "./src/e2k_js_bg.wasm.d.ts",
-  `declare const wasm: Uint8Array;export default wasm;`,
+  "declare const wasm: Uint8Array;export default wasm;",
 );
 
 await fs.promises.rm("./dist", { recursive: true, force: true });
 await tsdown.build({
   entry: {
-    "index": "./src/index.ts",
-    "sync": "./src/sync.ts",
+    index: "./src/index.ts",
+    sync: "./src/sync.ts",
   },
   outDir: "./dist",
   sourcemap: true,
