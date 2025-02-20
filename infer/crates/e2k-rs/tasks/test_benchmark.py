80842aa49b0bf3ec61df98fc56e976fab91e8b01
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-02-20 16:09:43 +0900

    fix: ベンチマークを直す

--- infer/crates/e2k-rs/tasks/test_benchmark.py
+++ infer/crates/e2k-rs/tasks/test_benchmark.py
@@ -5,8 +5,9 @@ import pytest
 def test_c2k(benchmark):
     c2k = e2k.C2K()
 
+    word = "constants"
     def bench_c2k():
-        c2k("constants")
+        c2k(word)
 
     benchmark.pedantic(bench_c2k, iterations=1, rounds=100)
 
@@ -14,8 +15,9 @@ def test_c2k(benchmark):
 def test_p2k(benchmark):
     p2k = e2k.P2K()
 
+    pron = ["K", "AA1", "N", "S", "T", "AH0", "N", "T", "S"]
     def bench_p2k():
-        p2k(["K", "AA1", "N", "S", "T", "AH0", "N", "T", "S"])  # type:ignore
+        p2k(pron)  # type:ignore
 
     benchmark.pedantic(bench_p2k, iterations=1, rounds=100)
 
