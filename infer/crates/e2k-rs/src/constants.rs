d23cff05588f3122c83b9e437ffcb113efe1291d
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-03-11 20:19:07 +0900

    chore: constantsを更新

--- infer/crates/e2k-rs/src/constants.rs
+++ infer/crates/e2k-rs/src/constants.rs
@@ -4,13 +4,13 @@
 // pub const PAD_IDX: usize = 0;
 pub const SOS_IDX: usize = 1;
 pub const EOS_IDX: usize = 2;
-pub const EN_PHONES: [&str; 73] = [
+pub const EN_PHONES: [&str; 74] = [
     "<pad>", "<sos>", "<eos>", "AA0", "AA1", "AA2", "AE0", "AE1", "AE2", "AH0", "AH1", "AH2",
     "AO0", "AO1", "AO2", "AW0", "AW1", "AW2", "AY0", "AY1", "AY2", "B", "CH", "D", "DH", "EH0",
     "EH1", "EH2", "ER0", "ER1", "ER2", "EY0", "EY1", "EY2", "F", "G", "HH", "IH0", "IH1", "IH2",
     "IY0", "IY1", "IY2", "JH", "K", "L", "M", "N", "NG", "OW0", "OW1", "OW2", "OY0", "OY1", "OY2",
     "P", "R", "S", "SH", "T", "TH", "UH0", "UH1", "UH2", "UW0", "UW1", "UW2", "V", "W", "Y", "Z",
-    "ZH", " ",
+    "ZH", " ", "'",
 ];
 pub const KANAS: [&str; 88] = [
     "<pad>", "<sos>", "<eos>", "ァ", "ア", "ィ", "イ", "ゥ", "ウ", "ェ", "エ", "ォ", "オ", "カ",
@@ -20,9 +20,7 @@ pub const KANAS: [&str; 88] = [
     "ボ", "ポ", "マ", "ミ", "ム", "メ", "モ", "ャ", "ヤ", "ュ", "ユ", "ョ", "ヨ", "ラ", "リ", "ル",
     "レ", "ロ", "ヮ", "ワ", "ヰ", "ヱ", "ヲ", "ン", "ヴ", "ー",
 ];
-pub const ASCII_ENTRIES: [&str; 56] = [
+pub const ASCII_ENTRIES: [&str; 31] = [
     "<pad>", "<sos>", "<eos>", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F",
-    "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y",
-    "Z", " ",
+    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "'",
 ];
