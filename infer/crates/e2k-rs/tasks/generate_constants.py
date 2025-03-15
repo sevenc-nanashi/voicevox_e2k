e13ebc60eaafa721da863808432af09e84ecd329
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-03-16 01:06:54 +0900

    chore: generate_constants内でfmtをかける

--- infer/crates/e2k-rs/tasks/generate_constants.py
+++ infer/crates/e2k-rs/tasks/generate_constants.py
@@ -1,11 +1,10 @@
 import os
 from e2k import constants
+import subprocess
 
 
 destination = os.path.join(os.path.dirname(__file__), "../src/constants.rs")
-with open(destination, "w") as f:
-    f.write(
-        f"""
+content = f"""
 // `task generate_constants` により生成。
 // このファイルは直接編集しないでください。
 
@@ -13,10 +12,17 @@ with open(destination, "w") as f:
 pub const SOS_IDX: usize = {constants.SOS_IDX};
 pub const EOS_IDX: usize = {constants.EOS_IDX};
 pub const KANAS: &[&str] = &[
-    {", ".join([f'"{kana}"' for kana in constants.kanas])}
+{", ".join([f'"{kana}"' for kana in constants.kanas])}
 ];
 pub const ASCII_ENTRIES: &[&str] = &[
-    {", ".join([f'"{entry}"' for entry in constants.ascii_entries])}
+{", ".join([f'"{entry}"' for entry in constants.ascii_entries])}
 ];
 """
-    )
+
+formatted_content = subprocess.run(
+    ["rustfmt", "--edition", "2021"],
+    input=content.encode(),
+    stdout=subprocess.PIPE,
+).stdout.decode()
+with open(destination, "w") as f:
+    f.write(formatted_content)
