b8111a589b7b555c0b56e605a266889fb288d73a
Nanashi. <sevenc7c@sevenc7c.com>
2025-03-17 13:00:14 +0900

    refactor: 関数を分ける
    Co-authored-by: Hiroshiba <hihokaruta@gmail.com>

--- dataset/src/tools/generateConfigSchema.ts
+++ dataset/src/tools/generateConfigSchema.ts
@@ -14,17 +14,24 @@ async function main() {
   ].join("\n");
 
   if (isCheck) {
-    const currentContent = await fs.readFile(path, "utf-8");
-    if (currentContent !== content) {
-      console.error(
-        `Config schema is out of date. Run 'pnpm run tools:generateConfigSchema' to update it.`,
-      );
-      process.exit(1);
-    }
-
-    console.log("Config schema is up to date.");
-    return;
+    await check(path, content)
+  } else {
+    await write(path, content)
   }
+}
+
+async function checkConfigSchema(path: string, content: string): Promise<void> {
+  const currentContent = await fs.readFile(path, "utf-8");
+  if (currentContent !== content) {
+    console.error(
+      `Config schema is out of date. Run 'pnpm run tools:generateConfigSchema' to update it.`,
+    );
+    process.exit(1);
+  }
+  console.log("Config schema is up to date.");
+}
+
+async function writeConfigSchema(path: string, content: string): Promise<void> {
   await fs.writeFile(path, content);
 }
 
