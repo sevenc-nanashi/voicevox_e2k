eaca9ad7738fa3c46e6ca3b2f8ad0ec857074aac
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-03-18 21:46:56 +0900

    feat: brotliをexportに含める

--- train/src/export.py
+++ train/src/export.py
@@ -5,18 +5,23 @@ Exports the torch weights
 import torch
 from safetensors.numpy import save_file
 import argparse
+import brotli
+import pathlib
+import yaml
 from train import Model
+from config import Config
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--model", type=str, required=True)
-parser.add_argument("--p2k", action="store_true")
 parser.add_argument("--output", type=str, required=True)
 parser.add_argument("--fp32", action="store_true")
 
 args = parser.parse_args()
 
-model = Model(p2k=args.p2k)
+config = pathlib.Path(args.model).parent / "config.yml"
+config = Config.from_dict(yaml.safe_load(config.open()))
+model = Model(config)
 model.load_state_dict(torch.load(args.model))
 model.eval()
 
@@ -36,3 +41,7 @@ output = (
     else f"{args.output}.safetensors"
 )
 save_file(weights, output)
+with open(output, "rb") as f:
+    compressed = brotli.compress(f.read())
+with open(output + ".br", "wb") as f:
+    f.write(compressed)
