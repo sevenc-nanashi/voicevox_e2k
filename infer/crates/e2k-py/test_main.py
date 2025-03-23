6e9f37b481946132816186d25abfc19cce808815
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-03-24 01:21:04 +0900

    feat: ベンチマークを追加

--- infer/crates/e2k-py/test_main.py
+++ infer/crates/e2k-py/test_main.py
@@ -6,3 +6,9 @@ def test_c2k():
 
     word = "constants"
     assert c2k(word) == "コンスタンツ"
+
+
+def test_c2k_benchmark(benchmark):
+    c2k = voicevox_e2k.C2k()
+    kata = benchmark(c2k, "constants")
+    assert kata == "コンスタンツ"
