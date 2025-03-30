ee45a4b3c78bc109f9fc572dfb8b1aa5d5365175
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-03-30 17:38:31 +0900

    chore: Finalをつける

--- infer/tools/common.py
+++ infer/tools/common.py
@@ -1,9 +1,10 @@
 from pathlib import Path
+from typing import Final
 import platform
 
-repo_root = Path(__file__).parent.parent.parent
-infer_root = repo_root / "infer"
-train_root = repo_root / "train"
+repo_root: Final[Path] = Path(__file__).parent.parent.parent
+infer_root: Final[Path] = repo_root / "infer"
+train_root: Final[Path] = repo_root / "train"
 
-is_windows = platform.system() == "Windows"
-is_linux = platform.system() == "Linux"
+is_windows: Final[bool] = platform.system() == "Windows"
+is_linux: Final[bool] = platform.system() == "Linux"
