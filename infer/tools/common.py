1f65eb5422c0d46de5d4f2c6d0977ad2f000563c
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-03-30 16:34:17 +0900

    chore: e2k-rs/tasksをtoolsに移動

--- infer/tools/common.py
+++ infer/tools/common.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 import platform
 
-infer_root = Path(__file__).parent.parent
+repo_root = Path(__file__).parent.parent.parent
+infer_root = repo_root / "infer"
+train_root = repo_root / "train"
 
 is_windows = platform.system() == "Windows"
 is_linux = platform.system() == "Linux"
