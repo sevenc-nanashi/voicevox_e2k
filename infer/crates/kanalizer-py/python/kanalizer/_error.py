b84d5e809bdca4997b2ad876346b676814b1fd9e
Nanashi. <sevenc7c@sevenc7c.com>
2025-04-17 22:34:43 +0900

    feat!(infer): Python側でエラーではなくwarningを吐けるように (#85)

--- infer/crates/kanalizer-py/python/kanalizer/_error.py
+++ infer/crates/kanalizer-py/python/kanalizer/_error.py
@@ -33,9 +33,6 @@ class EmptyInputError(InvalidInputError):
     入力が空文字列の場合に発生するエラー。
     """
 
-    def __init__(self, message: str):
-        super().__init__(message)
-
 
 class InvalidCharsError(InvalidInputError):
     """
@@ -50,3 +47,41 @@ class InvalidCharsError(InvalidInputError):
     def __init__(self, message: str, invalid_chars: list[str]):
         super().__init__(message)
         self.invalid_chars = invalid_chars
+
+
+class IncompleteConversionWarning(Warning):
+    """
+    変換が終了しなかった場合に発生する警告。
+
+    IncompleteConversionErrorとは異なり、incomplete_output属性は存在しない。
+    """
+
+
+class InvalidInputWarning(Warning):
+    """
+    無効な入力が与えられた場合に発生する警告の基底クラス。
+
+    See Also
+    --------
+    InvalidCharsWarning
+        無効な文字が含まれている場合に発生する警告。
+    EmptyInputWarning
+        入力が空文字列の場合に発生する警告。
+    """
+
+
+class EmptyInputWarning(InvalidInputWarning):
+    """
+    入力が空文字列の場合に発生する警告。
+    """
+
+
+class InvalidCharsWarning(InvalidInputWarning):
+    """
+    入力に無効な文字が含まれていた場合に発生する警告。
+
+    InvalidCharsErrorとは異なり、invalid_chars属性は存在しない。
+    """
+
+    def __init__(self, message: str):
+        super().__init__(message)
