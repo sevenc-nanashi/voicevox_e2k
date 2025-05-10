65fd7eee2d044b2663c6e1bad8a95281ddf72dc4
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-05-11 08:59:56 +0900

    refactor: コードをリファクタ

--- dataset/src/tools/mergeDatasets.ts
+++ dataset/src/tools/mergeDatasets.ts
@@ -1,42 +1,61 @@
+// データセットをマージするスクリプト。
+//
+// 使い方:
+// pnpm run tools:mergeDatasets dataset1.jsonl dataset2.jsonl ... output.jsonl
+//
 import fs from "node:fs/promises";
+import { z } from "zod";
 
-type DatasetLine = {
-  word: string;
-  kata: string[];
-};
+const datasetLineSchema = z.object({
+  word: z.string(),
+  kata: z.array(z.string()).length(1),
+});
+type DatasetLine = z.infer<typeof datasetLineSchema>;
 
 async function main() {
-  const datasets = process.argv.slice(2, -1);
+  const datasetPaths = process.argv.slice(2, -1);
   const outputPath = process.argv[process.argv.length - 1];
 
-  if (datasets.length === 0) {
+  if (datasetPaths.length === 0) {
     throw new Error("No dataset files provided.");
   }
 
-  const dataset = new Map<string, string>();
-  for (const datasetPath of datasets) {
-    const content = await fs.readFile(datasetPath, "utf-8");
-    const parsedContent = content
-      .split("\n")
-      .map((line) => JSON.parse(line)) as DatasetLine[];
-    for (const { word, kata } of parsedContent) {
-      if (kata.length !== 1) {
-        throw new Error(
-          `Invalid dataset: ${datasetPath}, word: ${word}, kata: ${kata}`,
-        );
-      }
-      dataset.set(word, kata[0]);
-    }
-  }
+  const dataset = await mergeDatasets(datasetPaths);
 
-  const outputContent = Array.from(dataset.entries())
-    .map(([word, kata]) => JSON.stringify({ word, kata: [kata] }))
-    .join("\n");
-  await fs.writeFile(outputPath, outputContent, "utf-8");
-  console.log(`Merged ${datasets.length} datasets into ${outputPath}`);
+  await writeDataset(dataset, outputPath, datasetPaths);
 }
 
 main().catch((err) => {
   console.error(err);
   process.exit(1);
 });
+
+async function mergeDatasets(datasets: string[]) {
+  const dataset = new Map<string, string>();
+  for (const datasetPath of datasets) {
+    const content = await fs.readFile(datasetPath, "utf-8");
+    const parsedContent = content
+      .split("\n")
+      .map((line) => datasetLineSchema.parse(JSON.parse(line)));
+
+    for (const { word, kata } of parsedContent) {
+      dataset.set(word, kata[0]);
+    }
+  }
+
+  return dataset;
+}
+
+async function writeDataset(
+  dataset: Map<string, string>,
+  outputPath: string,
+  datasets: string[],
+) {
+  const outputContent = Array.from(dataset.entries())
+    .map(([word, kata]) =>
+      JSON.stringify({ word, kata: [kata] } satisfies DatasetLine),
+    )
+    .join("\n");
+  await fs.writeFile(outputPath, outputContent, "utf-8");
+  console.log(`Merged ${datasets.length} datasets into ${outputPath}`);
+}
