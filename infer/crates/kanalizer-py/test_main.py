7e055a7d1ef8450a84c1b3bcfe13479925647a61
Nanashi. <sevenc7c@sevenc7c.com>
2025-05-17 21:36:06 +0900

    test: もう少し細かく
    Co-authored-by: Ryo Yamashita <qryxip@gmail.com>

--- infer/crates/kanalizer-py/test_main.py
+++ infer/crates/kanalizer-py/test_main.py
@@ -12,6 +12,7 @@ def test_kanalizer():
     "max_length, exception",
     [
         ("hoge", ValueError),
+        (0, ValueError),
         (-42, OverflowError),
         (2**64 + 1, OverflowError),
         (dict(), TypeError),
